9a3485a9b4bcf32bf35afddfdb1fabbf93fe0c99
Markus Weiß <114624948+Markus-G-S-Weiss@users.noreply.github.com>
2024-11-05 12:31:58 -0600

    Update opencubman.h

--- opencubman.h
+++ opencubman.h
@@ -1,5 +1,8 @@
 /* opencubman.h */
 /* Header file for the Cube class */
+/* Modified by Markus G. S. Weiss on 2024-11-04 */
+/* This file is part of OpenCubMan and is licensed under the GNU General Public License v2.0. */
+/* See the LICENSE and AUTHORS files for details. */
 
 #ifndef OPENCUBMAN_H
 #define OPENCUBMAN_H
